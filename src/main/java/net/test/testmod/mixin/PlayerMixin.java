331c3c49dbd6b7543249158b540a87c32a708d54
Lawliet <junirobert21@gmail.com>
2025-02-15 11:57:40 +0530

    removed unused imports

--- src/main/java/net/test/testmod/mixin/PlayerMixin.java
+++ src/main/java/net/test/testmod/mixin/PlayerMixin.java
@@ -1,17 +1,11 @@
 package net.test.testmod.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.mojang.authlib.GameProfile;
-import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodData;
-import net.minecraft.world.level.Level;
 import net.test.testmod.iterface.duck.FoodDataDuck;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Player.class)
 public abstract class PlayerMixin{
