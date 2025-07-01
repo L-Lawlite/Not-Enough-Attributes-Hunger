9681a001d31afc2d0a33003a9a40729a8ef58574
Lawliet <junirobert21@gmail.com>
2025-07-01 20:30:57 +0530

    Removed bus from events

--- src/main/java/net/lawliet/nea_hunger/compatibility/appleskin/AppleSkinEventHandler.java
+++ src/main/java/net/lawliet/nea_hunger/compatibility/appleskin/AppleSkinEventHandler.java
@@ -6,7 +6,7 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import org.slf4j.Logger;
 import squeek.appleskin.api.event.TooltipOverlayEvent;
 
-@EventBusSubscriber(modid = "appleskin", bus = EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(modid = "appleskin")
 public class AppleSkinEventHandler {
 
     private static final Logger LOGGER = LogUtils.getLogger();
