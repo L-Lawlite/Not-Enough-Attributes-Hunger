5df119819c48854cc5132f6c4eac74118b8c5183
Lawliet <junirobert21@gmail.com>
2025-07-02 01:26:58 +0530

    Fixed LocalPlayer constructor

--- src/main/java/net/lawliet/nea_hunger/mixin/LocalPlayerMixin.java
+++ src/main/java/net/lawliet/nea_hunger/mixin/LocalPlayerMixin.java
@@ -2,28 +2,20 @@ package net.lawliet.nea_hunger.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.mojang.authlib.GameProfile;
-import com.mojang.logging.LogUtils;
 import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.lawliet.nea_hunger.NeaHungerAttributes;
 import net.lawliet.nea_hunger.clientServerSync.ClientPacketListner;
-import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
 //@Debug(export = true)
 @Mixin(LocalPlayer.class)
 public abstract class LocalPlayerMixin extends Player{
-    public LocalPlayerMixin(Level level, BlockPos pos, float yRot, GameProfile gameProfile) {
-        super(level, pos, yRot, gameProfile);
+    public LocalPlayerMixin(Level level, GameProfile gameProfile) {
+        super(level, gameProfile);
     }
-
-
-    @Unique
-    private static final Logger nea_hunger$LOGGER = LogUtils.getLogger();
 
 
     @ModifyExpressionValue(method = "hasEnoughFoodToSprint", at = @At(value = "CONSTANT", args = "floatValue=6.0F"))
